d5f7e3edb941ee4a7aefaa20df278cda694a7bb3
zhuling822 <ling.zhu@js.design>
2023-11-15 20:53:43 +0800

    fix: 非递归方式

--- algorithmCode/tree/index.js
+++ algorithmCode/tree/index.js
@@ -15,64 +15,113 @@ class BinarySerachTree {
     this.root = null;
   }
 
-  // 先序创建二叉树的函数: 根左右
+  // =========================创建===============================
+  // (递归-先序)先序创建二叉树的函数: 根左右,每一颗子树都是一样的递归创建，顺序是排好的
   preCreateBinaryTree(preOrder) {
-    if (preOrder.length === 0) return;
-    const root = new Node(preOrder[0]);
+    let index = 0;
+    function buildTree() {
+      const value = preOrder[index++];
 
-    const left = [];
-    const right = [];
+      if (value === '#' || value === undefined) {
+        return null;
+      }
+      // 根左右
+      const newNode = new Node(parseInt(value));
+      newNode.left = buildTree();
+      newNode.right = buildTree();
 
-    // 左子树都比根节点小
-    for (let i = 1; i < preOrder.length; i++) { 
-      if (preOrder[i] < root.data) {
-        left.push(preOrder[i]);
-      }
+      return newNode;
     }
-    root.left = this.preCreateBinaryTree(left);
-    // 右子树都比根节点大
-    for (let i = 1; i < preOrder.length; i++) { 
-      if (preOrder[i] > root.data) {
-        right.push(preOrder[i]);
-      }
-    }
-    root.right = this.preCreateBinaryTree(right);
-    return root;
+
+    return buildTree();
   }
-}
-const preOrder = [6, 4, 3, 5, 9, 10, 11];
-const preTree = new BinarySerachTree();
-preTree.preCreateBinaryTree(preOrder);
-console.log("✅ ~ zhuling preTree.preCreateBinaryTree(preOrder)", preTree.preCreateBinaryTree([6,4,3,5,9,10,11]))
 
-// {
-//   "data": 6,
-//   "left": {
-//     "data": 4,
-//     "left": {
-//       "data": 3,
-//       "left": undefined,
-//       "right": undefined
-//     },
-//     "right": {
-//       "data": 5,
-//       "left": undefined,
-//       "right": undefined
-//     }
-//   },
-//   "right": {
-//     "data": 9,
-//     "left": undefined,
-//     "right": {
-//       "data": 10,
-//       "left": undefined,
-//       "right": [
-//         {
-//           "data": 1,
-//           "left": undefined,
-//           "right": undefined
-//         }
-//       ]
-//     }
-//   }
-// }
+  // (非递归-先序): 用栈模拟递归的过程
+  noRePreCreateBinaryTree(preOrder) {
+    // 数组长度为0
+    if (!preOrder || preOrder.length === 0) {
+      return null;
+    }
+
+    // 创建根节点并将其推入栈中
+    const root = new Node(parseInt(preOrder[0]));
+    const stack = [];
+    stack.push(root);
+    let index = 1;
+    let currentNode = root;
+
+    while (index < preOrder.length) {
+      // 当前值不是 '#'，创建新节点并连接到当前节点的左/右子树
+      if (preOrder[index] !== '#') { 
+        const newNode = new Node(parseInt(preOrder[index]));
+        // 如果当前节点的左孩子为空，将新节点作为当前节点的左孩子
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+        } else {
+          currentNode.right = newNode;
+        }
+        // 将新节点推入栈中
+        stack.push(newNode); 
+        // 更新当前节点为新节点，继续向下处理
+        currentNode = newNode; 
+      } else { 
+        // 当前值为 '#'，说明当前节点的子节点为空
+        if (!currentNode.left) {
+          currentNode.left = null; // 将左子节点设为 null
+        } else {
+          currentNode.right = null; // 将右子节点设为 null
+        }
+        // 找到最近一个非空节点，即栈中最后一个节点
+        while (preOrder[index] === '#' && stack.length > 0) {
+          currentNode = stack.pop(); // 弹出栈顶节点
+        }
+        index--; // 因为要处理当前为 '#' 的节点，所以需要回退一个位置
+      }
+      index++;
+    }
+    return root; // 返回根节点
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  // (递归)先序遍历输出
+  preOrderPrintf(node) {
+    if (node === null) {
+      return;
+    }
+    console.log(node.data);
+    this.preOrderPrintf(node.left);
+    this.preOrderPrintf(node.right);
+  }
+
+  // 非递归先序创建二叉树
+
+}
+
+// 非递归先序创建
+const preorderTraversal = ['1', '2', '4', '#', '#', '#', '3', '5', '#', '#', '6', '#', '#'];
+const preTree = new BinarySerachTree();
+
+// const preOrderNode = preTree.preCreateBinaryTree(preorderTraversal)
+// console.log("✅ ~ zhuling （递归）preOrderNode,先序序列创建的树", preOrderNode)
+// console.log("✅ ~ zhuling （递归）preOrderTraversalCheck:", preTree.preOrderPrintf(preOrderNode))
+
+
+
+const nRePreOrderNode = preTree.noRePreCreateBinaryTree(preorderTraversal);
+
+console.log("✅ ~ zhuling （非递归）nRePreOrderNode:", nRePreOrderNode)
+console.log("✅ ~ zhuling （非递归）preOrderTraversalCheck:", preTree.preOrderPrintf(nRePreOrderNode))
+
+
+
+
