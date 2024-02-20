3b6d7d5178546e00421b376f5186970620e574c4
zhuling904 <ling.zhu.904@gmail.com>
2024-02-20 23:21:35 +0800

    fix: 集合类型数组方法

--- code/04手写数组方法/01集合类/02map.js
+++ code/04手写数组方法/01集合类/02map.js
@@ -1,37 +1,39 @@
 // 帮助函数：它能更好地解释每个方法的功能:接受什么作为输入，返回什么，以及它是否对数组进行了修改。
 function logHelper(operateName, array, callback) {
-    const input = [...array];
-    const result = callback(array);
+  const input = [...array];
+  const result = callback(array);
 
-    console.log({
-        operation: operateName,
-        arrayBefore: input,
-        arrayAfter: array,
-        // 是否修改数组
-        mutates: mutatesArray(input, array),
-        result,
-    })
+  console.log({
+    operation: operateName,
+    arrayBefore: input,
+    arrayAfter: array,
+    // 是否修改数组
+    mutates: mutatesArray(input, array),
+    result,
+  });
 }
 
-// 浅比较判断是否修改了数组 
+// 浅比较判断是否修改了数组
 function mutatesArray(array1, array2) {
-    if (array1.length !== array2.length) {
-        return true
+  if (array1.length !== array2.length) {
+    return true;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return true;
     }
-    for(let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-            return true
-        }
-    }
-    return false;
+  }
+  return false;
 }
 
 Array.prototype.myMap = function (array, callback) {
-    const result = [];
-    for(let i = 0; i < array.length; i++) {
-        result[i] = callback(array[i], i, array);
-    }
-    return result;
-}
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result[i] = callback(array[i], i, array);
+  }
+  return result;
+};
 
-logHelper('forEach', [1, 2, 3, 4, 5], array => Array.prototype.myMap(array, value => value * 2));
+logHelper("map", [1, 2, 3, 4, 5], (array) =>
+  Array.prototype.myMap(array, (value) => value * 2)
+);
--- code/04手写数组方法/01集合类/index.js
+++ code/04手写数组方法/01集合类/index.js
@@ -1,14 +1,88 @@
-const BasicArray = require('../00实现数组结构/index')
+const BasicArray = require("../00实现数组结构/index");
 class CollectArray extends BasicArray {
-    log() {
-        console.log('测试方法', this.getLength());
+  initArr(arr) {
+    this.data = arr;
+  }
+
+  myForEach(callback, thisValue) {
+    const array = thisValue ? thisValue.data : this.data;
+    // `forEach()` 对于空数组是不会执行回调函数的。
+    if (array.length === 0) return;
+    // 遍历次数再第一次循环前就会确定，再添加到数组中的元素不会被遍历。
+    for (let i = 0; i < array.length; i++) {
+      // 它总是返回 undefined值,即使你return了一个值。
+      // 如果已经存在的值被改变，则传递给 callback 的值是遍历到他们那一刻的值。
+      array[i] && callback(array[i], i, array);
     }
+  }
+
+  myMap(callback, thisValue) {
+    const result = [];
+    const array = thisValue ? thisValue.data : this.data;
+    if (array.length === 0) return [];
+    for (let i = 0; i < array.length; i++) {
+      result[i] = callback(array[i], i, array);
+    }
+    // 方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值，并没有改变原来的数组。
+    return result;
+  }
+
+  myFilter(callback, thisValue) {
+    const result = [];
+    const array = thisValue ? thisValue.data : this.data;
+    if (array.length === 0) return [];
+    for (let i = 0; i < array.length; i++) {
+      if (callback(array[i], i, array)) result.push(array[i]);
+    }
+    return result;
+  }
+
+  myReduce(callback, initValue, thisValue) {
+    const array = thisValue ? thisValue.data : this.data;
+    // 如果传递初始值，从0开始
+    let start = 0;
+    let acc = initValue;
+    if (array.length === 0) return acc;
+    // 如果不传递初始值，从1开始
+    if (initValue === undefined) {
+      acc = array[0];
+      start = 1;
+    }
+    for (let i = start; i < array.length; i++) {
+      acc = callback(acc, array[i], i, array);
+    }
+    return acc;
+  }
 }
 
 const array = new CollectArray();
+// 初始化一个数组
+array.initArr([1, 2, 3, 4, 5, 6, 7, 8]);
 
-array.add(1);
-array.add(2);
-array.add(3);
+// forEach
+array.logHelper("forEach", array.getArray(), () => {
+  array.myForEach((item, index, array) => {
+    // console.log("✅ ~ item:", item);
+  });
+});
 
-array.log();
+// map
+array.logHelper("map", array.getArray(), () => {
+  return array.myMap((item, index, array) => {
+    return item * 2;
+  });
+});
+
+// filter
+array.logHelper("filter", array.getArray(), () => {
+  return array.myFilter((item, index, array) => {
+    return item < 4;
+  });
+});
+
+// reduce
+array.logHelper("reduce", array.getArray(), () => {
+  return array.myReduce((pre, item, index, array) => {
+    return pre * item;
+  }, 0);
+});
